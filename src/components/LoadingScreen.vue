829b5adaacf2f3eb990925fb8742340959424a7c
Ulysseee <62332779+Ulysseee@users.noreply.github.com>
2022-04-19 15:26:47 +0200

    [feat] clean loader

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -1,25 +1,41 @@
 <template>
 	<div ref="loadingScreen" class="loadingScreen">
 		<div class="wrapper">
-			<h2>Loading</h2>
-			<p class="loading__description">
-				Loading different textures and models
-			</p>
-			<div class="progressBar">
-				<div
-					class="progressFill"
-					:style="{ maxWidth: `${progress}%`, width: `${progress}%` }"
-				></div>
-			</div>
-			<div class="progressUrl">{{ progressUrl }}</div>
+			<h3 ref="progressUrl" class="progressUrl">
+				{{ progressUrl }}
+			</h3>
 		</div>
 
-		<p class="percent">{{ progress.toFixed(0) }}%</p>
+		<div class="percent-wrapper">
+			<p ref="percent">{{ progress.toFixed(0) }}%</p>
+		</div>
+
+		<svg
+			class="overlay"
+			width="100%"
+			height="100%"
+			viewBox="0 0 100 100"
+			preserveAspectRatio="none"
+		>
+			<path
+				ref="overlay__top"
+				class="overlay__top"
+				vector-effect="non-scaling-stroke"
+				d="M 0 0 V 51 Q 50 51 100 51 V 0 z"
+			/>
+			<path
+				ref="overlay__bottom"
+				class="overlay__bottom"
+				vector-effect="non-scaling-stroke"
+				d="M 0 100 V 50 Q 50 50 100 50 V 100 z"
+			/>
+		</svg>
 	</div>
 </template>
 
 <script>
 import Experience from '../Experience/Experience'
+import gsap, { Power3 } from 'gsap'
 
 export default {
 	name: 'LoadingScreen',
@@ -39,9 +55,30 @@ export default {
 		})
 
 		this.experience.resources.on('ready', () => {
-			setTimeout(() => {
-				this.$refs.loadingScreen.classList.add('finished')
-			}, 1000)
+			const spans = gsap.utils.selector(this.$refs.letters)
+			console.log(spans('.letter'))
+
+			gsap.timeline()
+				.to([this.$refs.progressUrl, this.$refs.percent], {
+					y: -85,
+					duration: 1.2,
+					ease: Power3.easeInOut
+				})
+				.to(this.$refs.overlay__top, {
+					duration: 0.9,
+					ease: Power3.easeInOut,
+					attr: { d: 'M 0 0 V 0 Q 50 0 100 0 V 0 z' }
+				})
+				.to(this.$refs.overlay__bottom, {
+					duration: 0.9,
+					ease: Power3.easeInOut,
+					delay: -0.9,
+					attr: { d: 'M 0 100 V 100 Q 50 100 100 100 V 100 z' }
+				})
+				.to(this.$refs.loadingScreen, {
+					css: { opacity: '0', pointerEvents: 'none' },
+					ease: Power3.easeOut
+				})
 		})
 	}
 }
@@ -51,7 +88,6 @@ export default {
 .loadingScreen {
 	width: 100vw;
 	height: 100vh;
-	background: #151515;
 	position: absolute;
 	top: 0;
 	left: 0;
@@ -61,46 +97,49 @@ export default {
 	transition: opacity 0.5s;
 	display: flex;
 	align-items: center;
+	font-family: 'Mak';
+
+	.overlay {
+		position: absolute;
+		pointer-events: none;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+	}
 
 	.wrapper {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		margin: auto;
+		overflow: hidden;
 	}
 	h2 {
 		font-size: 30px;
 	}
-	.loading__description {
-		font-size: 16px;
-	}
-	.progressBar {
-		width: 500px;
-		height: 5px;
-		background: black;
-		border-radius: 30px;
-		margin: 20px 0;
-	}
 	.progressUrl {
 		color: gray;
-		margin-top: 20px;
 		font-size: 0.5em;
-		height: 25px;
+		letter-spacing: 0.75px;
+		z-index: 2;
+		overflow: hidden;
+		margin: 0;
 	}
-	.progressFill {
-		width: 0%;
-		height: 100%;
-		background: #bc13fe;
-		transition: all 0.3s;
-		border-radius: 30px;
+	.letter {
+		display: inline-block;
+		will-change: transform;
 	}
-	.percent {
+	.percent-wrapper {
 		position: absolute;
 		color: gray;
-		bottom: 40px;
+		bottom: 30px;
 		font-size: 16px;
-		left: 50%;
-		transform: translate(-50%, 0);
+		right: 30px;
+		margin: 0;
+		letter-spacing: 0.5px;
+		z-index: 2;
+		overflow: hidden;
 	}
 	&.finished {
 		opacity: 0;
